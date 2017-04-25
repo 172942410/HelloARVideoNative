69161aca22085e0142e9532a2d919a2d0fffade9
李鹏军 <172942410@163.com>
2017-04-25 10:11:39 +0800

    Initial commit

---
+++ app/src/main/obj/local/armeabi-v7a/objs/HelloARNative/ar.o.d
@@ -0,0 +1,50 @@
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/obj/local/armeabi-v7a/objs/HelloARNative/ar.o: \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/ar.cc \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/ar.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/camera.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/base.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/matrix.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/imagetracker.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/augmenter.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/image.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/imagetarget.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/target.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/storage.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/frame.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/player.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/utility.hpp \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/android/log.h
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/ar.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/camera.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/base.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/matrix.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/imagetracker.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/augmenter.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/image.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/imagetarget.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/target.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/../../../../../../package/include/easyar/storage.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/frame.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/player.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/utility.hpp:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/android/log.h:
---
+++ app/src/main/obj/local/armeabi-v7a/objs/HelloARNative/renderer.o.d
@@ -0,0 +1,32 @@
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/obj/local/armeabi-v7a/objs/HelloARNative/renderer.o: \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/renderer.cc \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/renderer.hpp \
+ /Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/matrix.hpp \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/GLES2/gl2.h \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/GLES2/gl2platform.h \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/KHR/khrplatform.h \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/sys/cdefs.h \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/android/api-level.h \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/stdint.h \
+ /Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/renderer.hpp:
+
+/Users/lipengjun/Desktop/work/AR/EasyARSDKSamples/Android/HelloARVideoNative/app/src/main/jni/easyar/matrix.hpp:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/GLES2/gl2.h:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/KHR/khrplatform.h:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/android/api-level.h:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/stdint.h:
+
+/Users/lipengjun/Desktop/work/dev-tools/android/ndk/android-ndk-r10/platforms/android-L/arch-arm/usr/include/machine/wchar_limits.h:
