a404353cc5de638c886ff61b0aaf23013b59cff6
李鹏军 <172942410@163.com>
2017-04-26 14:52:07 +0800

    添加本地准备界面框架

---
+++ app/src/main/java/cn/easyar/samples/helloarvideo/ReadyActivity.java
@@ -0,0 +1,69 @@
+package cn.easyar.samples.helloarvideo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import cn.easyar.adapter.ReadyRecyclerAdapter;
+
+
+/**
+ *
+ * @author lipengjun 2017-04-26
+ */
+public class ReadyActivity extends ActionBarActivity implements View.OnClickListener{
+
+    RecyclerView recyclerView;
+    ReadyRecyclerAdapter readyRecyclerAdapter;
+    Button buttonAdd,buttonStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ready);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        buttonAdd = (Button) findViewById(R.id.button_add);
+        buttonStart = (Button) findViewById(R.id.button_start);
+
+        buttonAdd.setOnClickListener(this);
+        buttonStart.setOnClickListener(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        readyRecyclerAdapter = new ReadyRecyclerAdapter();
+        recyclerView.setAdapter(readyRecyclerAdapter);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onClick(View v) {
+    int id = v.getId();
+        switch (id){
+            case R.id.button_add:
+
+                break;
+            case R.id.button_start:
+
+                break;
+
+        }
+    }
+
+}
